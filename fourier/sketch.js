2820411801ef8799a28b976f5d9d1b2bc1a3a6dd
shinohara-yusuke <shinohara.yusuke@vega-c.com>
2019-03-11 16:45:32 +0900

    mod scale

--- fourier/sketch.js
+++ fourier/sketch.js
@@ -1,5 +1,6 @@
 var step = 0.01;
 var yo;
+var x;
 var y;
 var t = 0;
 
@@ -9,8 +10,11 @@ function setup() {
 
   yo = new Array(width);
   y = new Array(width);
+  x = new Array(width);
 
   for (var i = 0; i < y.length; i++) {
+    x[i] = i * step;
+
     if (
       (i >= 0 && i < 100) ||
       (i >= 200 && i < 300) ||
@@ -20,18 +24,18 @@ function setup() {
       (i >= 1000 && i < 1100) ||
       (i >= 1200 && i < 1300)
     ) {
-      yo[i] = 100;
+      yo[i] = 1;
     } else {
-      yo[i] = 0;
+      yo[i] = -1;
     }
   }
 
-  drawArray(yo);
+  drawArray(x, yo);
 }
 
 function draw() {
   if (t == 0) {
-    var a0 = yo.reduce((arg1, arg2) => arg1 + arg2) / yo.length;
+    var a0 = yo.reduce((arg1, arg2) => arg1 + arg2) / width;
 
     for (var i = 0; i < y.length; i++) {
       y[i] = a0;
@@ -41,25 +45,28 @@ function draw() {
     var bn = 0;
 
     for (var i = 0; i < y.length; i++) {
-      an += y[i] * cos((2 * PI * t * i * step) / width) * 2 / width;
-      bn += y[i] * sin((2 * PI * t * i * step) / width) * 2 / width;
+      an +=
+        (yo[i] * cos((2 * PI * t * x[i]) / (width * step)) * 2) / (width * step);
+      bn +=
+        (yo[i] * sin((2 * PI * t * x[i]) / (width * step)) * 2) / (width * step);
     }
 
     for (var i = 0; i < y.length; i++) {
       y[i] +=
-        an * cos((2 * PI * t * i * step) / width) + bn * sin((2 * PI * t * i * step) / width);
+        an * cos((2 * PI * t * x[i]) / (width * step)) +
+        bn * sin((2 * PI * t * x[i]) / (width * step));
     }
 
-    console.log(an + bn);
+    console.log(bn);
   }
 
-  drawArray(y);
+  drawArray(x, y);
   t++;
 }
 
-function drawArray(array) {
-  for (var i = 0; i < array.length; i++) {
-    pp(i, array[i]);
+function drawArray(xArray, yArray) {
+  for (var i = 0; i < xArray.length; i++) {
+    pp(xArray[i], yArray[i]);
   }
 }
 
@@ -69,9 +76,9 @@ function pp(x, y) {
 
 function cx(x) {
   // return x + width / 2;
-  return x;
+  return x / step;
 }
 
 function cy(y) {
-  return height / 2 - y;
+  return height / 2 - y / step;
 }
