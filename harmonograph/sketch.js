43a8be1874d662f370ae00f465eeca42afaac160
Yusuke Shinohara <complex.alpha.cxa@gmail.com>
2020-01-17 11:21:52 +0900

    slider

--- harmonograph/sketch.js
+++ harmonograph/sketch.js
@@ -1,10 +1,15 @@
 const s = 200;
-var cnt = 1;
+let wSliders = new Array(4);
 
 function setup() {
   createCanvas(1200, 800);
   background(255);
 
+  for(let c = 0; c < wSliders.length; c++){
+    wSliders[c] = createSlider(0, 100, 10, 1);
+    wSliders[c].position(1000, 100 * (c + 1));
+    wSliders[c].style('width', '200px');
+  }
 }
 
 function draw() {
@@ -13,20 +18,21 @@ function draw() {
 
   for(let t = 0; t < 1000; t += 0.01){
 
-    stroke(cos(t) * 255, sin(t) * 255, sin(t) * 255);
+    stroke(cos(t) * 255, cos(t + 2 * PI / 3) * 255, cos(t + 4 * PI / 3) * 255);
 
-    const w1 = 11;
-    const w2 = 13;
-    const w3 = 13;
-    const w4 = 17;
+    let ws = new Array(wSliders.length);
+    for(let c = 0; c < wSliders.length; c++){
+
+      ws[c] = wSliders[c].value();
+    }
 
     const l1 = 1;
     const l2 = 1;
     const l3 = 1;
     const l4 = 1;
 
-    var x = wavePosition(t, w1, l1) + wavePosition(t, w2, l2);
-    var y = wavePosition(t, w3, l3) + wavePosition(t, w4, l4);
+    var x = wavePosition(t, ws[0], l1) + wavePosition(t, ws[1], l2);
+    var y = wavePosition(t, ws[2], l3) + wavePosition(t, ws[3], l4);
 
     plotGraph(x, y);
   }
