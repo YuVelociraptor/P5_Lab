f10fb6c2a3e1a682a087a8174c70eb82f5b1080a
Yusuke Shinohara <complex.alpha.cxa@gmail.com>
2024-07-27 20:55:39 +0900

    writer

--- contents/write/sketch.js
+++ contents/write/sketch.js
@@ -1,4 +1,4 @@
-let cnt = 0;
+let answer = '';
 
 const sketch001 = function (p) {
 
@@ -10,36 +10,77 @@ const sketch001 = function (p) {
 
     p.draw = function () {
         p.background(255, 255, 0);
-        p.ellipse(10, 10, 10, 10)
+        p.textSize(32);
+        p.text(answer, 10, 30);
     };
 
     p.mouseClicked = function () {
         if(p.mouseX > 0 && p.mouseX < width && p.mouseY > 0 && p.mouseY < height) {
-            cnt++;
+
         }
     };
 };
 
 const sketch002 = function (p) {
-    const width = 500;
-    const height = 500;
+    const width = 300;
+    const height = 300;
 
     p.setup = function () {
         p.createCanvas(width, height);
+        p.background(255, 255, 255);
     };
 
     p.draw = function () {
-        p.background(255, 0, 255);
-        p.ellipse(10, 10, 10, 10);
-        p.text(cnt.toString(), 40, 40);
+        if (p.mouseIsPressed) {
+            p.stroke(0);
+            p.strokeWeight(4);
+            p.line(p.mouseX, p.mouseY, p.pmouseX, p.pmouseY);
+        }
     };
 
     p.mouseClicked = function () {
         if(p.mouseX > 0 && p.mouseX < width && p.mouseY > 0 && p.mouseY < height) {
-            cnt--;
+
         }
     };
 };
 
+const sketch003 = function (p) {
+    const width = 300;
+    const height = 300;
+
+
+    p.setup = function () {
+        p.createCanvas(width, height);
+        p.background(255, 255, 255);
+    };
+
+    p.draw = function () {
+        p.rect(30, 20, 55, 40);
+    };
+
+    p.mouseClicked = function () {
+        if(p.mouseX > 0 && p.mouseX < width && p.mouseY > 0 && p.mouseY < height) {
+            const canvasElement = document.querySelector('canvas');
+            const dataURL = canvasElement.toDataURL('image/png');
+
+            Tesseract.recognize(
+                dataURL,
+                'eng',
+                {
+                    //logger: (m) => console.log(m),
+                }
+            ).then(({ data: { text } }) => {
+                //answer = text;
+                console.log(text);
+            });
+
+            answer = 'aaaa';
+        }
+    };
+
+}
+
 new p5(sketch001, 'canvas001');
 new p5(sketch002, 'canvas002');
+new p5(sketch003, 'canvas003');
